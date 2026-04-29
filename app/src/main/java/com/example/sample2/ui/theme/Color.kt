3fbd28485e34ed9f0ae0bb0b64530e2eff11fb14
wineda <132287567+wineda@users.noreply.github.com>
2026-04-29 14:59:24 +0900

    feat: emphasize parent timestamp and hide child timestamp

--- app/src/main/java/com/example/sample2/ui/theme/Color.kt
+++ app/src/main/java/com/example/sample2/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val CategoryWork = Color(0xFF2D6A4F)
+val CategoryExerciseBody = Color(0xFFD97706)
+val CategorySleep = Color(0xFF4338CA)
+val CategoryMorningHabit = Color(0xFF7C3AED)
+val CategoryEmotionNegative = Color(0xFFB91C1C)
+val CategoryEmotionPositive = Color(0xFF15803D)
+
