98a705eca7341a93fd2fd86a7cd23967742b6a2d
wineda <132287567+wineda@users.noreply.github.com>
2026-05-03 10:00:26 +0900

    Fix missing height import in Components

--- app/src/main/java/com/example/sample2/ui/Components.kt
+++ app/src/main/java/com/example/sample2/ui/Components.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
